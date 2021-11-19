6581795a54a55586e7787298aef61fef555471db
ayoub benouahi <abenouahi5@gmail.com>
2021-11-19 17:03:50 +0100

    intial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int rechirchesimple(int val,int T[],int n);
+
+int main()
+{
+    int T[]
+    return 0;
+}
+
+int rechirchesimple(int val,int T[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(val == T[i])
+        {
+
+        }
+    }
+}
